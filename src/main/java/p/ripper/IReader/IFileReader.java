372cecc48b7e23cf024ed8c88ff7a40d93f120ed
RockTraveler <atk2009@126.com>
2015-07-14 22:14:13 +0800

    edit the interface comment
    edit the interface comment

--- src/main/java/p/ripper/IReader/IFileReader.java
+++ src/main/java/p/ripper/IReader/IFileReader.java
@@ -3,7 +3,7 @@ package p.ripper.IReader;
 import java.io.File;
 
 public interface IFileReader {
-	
+	//define a reader which implement method could be use for reading a file
 	String reader(File file) throws Exception;
 
 }
